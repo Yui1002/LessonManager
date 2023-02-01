0933edefd0c02def653571dae9f164e91d15b550
Yui Murayama <yuimurayama1002@gmail.com>
2023-02-01 00:49:31 -0800

    add style

--- client/src/components/PastClass.jsx
+++ client/src/components/PastClass.jsx
@@ -10,9 +10,12 @@ import {
   Paper,
 } from "@mui/material";
 import moment from "moment";
+import "./PastClass.css";
+import {useNavigate} from "react-router-dom";
 
 const PastClass = () => {
   const [pastClasses, setPastClasses] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     getPastClasses();
@@ -20,14 +23,24 @@ const PastClass = () => {
 
   const getPastClasses = () => {
     axios.get("/schedule/past").then((res) => {
-      console.log(res.data);
+      console.log("1: ", res.data);
+      res.data.sort((a, b) => {
+        return new Date(b.start_date) - new Date(a.start_date);
+      });
+      console.log("2: ", res.data);
       setPastClasses(res.data);
     });
   };
 
   return (
     <div>
-      <h2>Past class</h2>
+      <button
+        className="profile_go_back_button"
+        onClick={() => navigate("/home")}
+      >
+        Go Back
+      </button>
+      <h2 className="past_class_title">Past classes</h2>
       <TableContainer>
         <Table sx={{ minWidth: 500 }} aria-label="simple table">
           <TableHead>
@@ -43,8 +56,10 @@ const PastClass = () => {
                 sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
               >
                 <TableCell component="th" scope="row">
-                  {moment(c.start_date).format("YYYY/MM/DD")} <br/>
-                  {`${moment(c.start_date).format("HH:mm")} - ${moment(c.end_date).format("HH:mm")}`} 
+                  {moment(c.start_date).format("YYYY/MM/DD")} <br />
+                  {`${moment(c.start_date).format("HH:mm")} - ${moment(
+                    c.end_date
+                  ).format("HH:mm")}`}
                 </TableCell>
                 <TableCell>{c.name}</TableCell>
               </TableRow>
